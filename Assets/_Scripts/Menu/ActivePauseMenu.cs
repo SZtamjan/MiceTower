548d3cc044558e88065876f50ed6e1a1ebd3adcb
nrtqq <nrtqqgaming@gmail.com>
2024-06-30 16:50:53 +0200

    Dodanie MainMenu oraz SettingsMenu import assetow stoneUI

---
+++ Assets/_Scripts/Menu/ActivePauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActivePauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+  
+    
+    void Start()
+    {
+        
+        pauseMenu.SetActive(false);
+        
+    }
+    
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+       
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    
+    
+    
+    
+    
+}
